1c3cb55deea3f6299185a5c573869e42346e5bcd
Moiseev Aleksej <moiseev.003@bk.ru>
2022-03-26 20:42:46 +0300

    Add homework tasck

--- adapters/repo.py
+++ adapters/repo.py
@@ -1,7 +1,7 @@
 from classic.components.component import component
 from classic.sql_storage.repository import BaseRepository
 from sqlalchemy import select, desc, text, func
-from adapters.tables import employees
+from adapters.tables import employees, departments
 
 from aplications.dataclases import Chat, Employee
 from aplications.interface import RepositoryInterface
@@ -32,8 +32,14 @@ class EmployeeRepository(SQLiteRepository, BaseRepository):
     model = Employee
 
     def get_departments_performance_sum(self):
-        query_text = text("select department_id, sum(performance) from employee group by department_id")
-        return self.session.execute(query_text).all()
+        query_text_join = text("select department.name as department, sum(performance)"
+                               " from employee  join department on employee.department_id=department.id"
+                               " group by department.name")
+        query = select(
+            departments.c.name,
+            func.sum(employees.c.performance)
+        ).select_from(employees.join(departments)).group_by(departments.c.name)
+        return self.session.execute(query).all()
 
     def get_most_successfully_employees(self):
         query = (select(self.model).order_by(desc(self.model.performance))
